44312884cfe729022856115888647888318eefc9
bryantobing12 <bryantobing0@gmail.com>
2021-02-10 00:24:17 +0700

    Fix: typo text youtube endpoint

--- app.test.js
+++ app.test.js
@@ -5,6 +5,6 @@ const request = supertest(app);
 test('Call the /youtube endpoint', async (done) => {
   const res = await request.get('/youtube');
   expect(res.status).toBe(200);
-  expect(res.text).toBe('Hello, Youtub');
+  expect(res.text).toBe('Hello, Youtube');
   done();
 });
